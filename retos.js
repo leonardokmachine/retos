77626c04c7dd6be97d621a6f96bfdb3c2cd95bdd
leonardokmachine <leonardogbr3@gmail.com>
2024-06-05 07:58:23 -0500

    Update retos.js

--- retos.js
+++ retos.js
@@ -1,10 +1,8 @@
 // PROGRAMA PARA SUMAR DOS NUMEROS
-/*
+
 let a= prompt("Ingrese primer Numero");
 let b= prompt("Ingrese segundo Numero");
 let resultado= a+b;
 alert(resultado);
 console.log(resultado);
-/*
 
-// PROGRAMA PARA CREAR UNA CALCULADORA
